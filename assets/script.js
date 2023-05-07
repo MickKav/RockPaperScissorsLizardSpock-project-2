43ff80e23ad1d5759d6d64b14371ebe1f0f866bf
MickKav <>
2023-05-07 20:41:32 +0000

    Write function structure in JS for game

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// function to generate a random choice for the computer
+function computerPlay() {
+
+}
+
+// function to play one round of the game
+function playRound() {
+
+}
+
+// add event listeners to the buttons
