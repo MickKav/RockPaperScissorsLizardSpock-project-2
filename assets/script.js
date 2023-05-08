4cb1ea94377b42b96819d653749cb627a4b476d4
MickKav <>
2023-05-08 18:08:34 +0000

    Create functions for computerPlay & playRound

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
 // function to generate a random choice for the computer
 function computerPlay() {
-
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
 // function to play one round of the game
-function playRound() {
-
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) 
+    {
+        userScore++;
+        return "You win!";
+    } else {
+        computerScore++;
+        return "You lose!";
+    }
 }
 
 // add event listeners to the buttons
