8bc57fb4c693551006a67be3ba175b97378521a2
MickKav <>
2023-05-08 18:35:05 +0000

    Add querySelectors, if else statements and other key info to functions

--- assets/script.js
+++ assets/script.js
@@ -29,16 +29,37 @@ function playRound(playerSelection, computerSelection) {
 
 // function to update the lives display
 function updateLives() {
-    
+    let livesDisplay = '';
+    for (let i = 0; i < lives; i++) {
+    livesDisplay += '<span class="heart">♥</span>';
+    }
+    document.querySelector('#lives').innerHTML = livesDisplay;
     }
 
     // function to reset the game
 function resetGame() {
-    
+    lives = 3;
+    userScore = 0;
+    computerScore = 0;
+
+    updateLives();
+    document.querySelector('#result-notice-text').textContent = 'Choose your weapon!';
+    document.querySelector('#result-image').innerHTML = '';
+    document.querySelector('#win-lose-text').textContent = '';
+    document.querySelector('#result').classList.remove('hidden');
     }
 
     // function to end the game
 function endGame() {
-    
+    document.querySelector('#result-notice-text').textContent = 'Game over!';
+    document.querySelector('#result-image').innerHTML = '';
+
+    if (userScore > computerScore) {
+        document.querySelector('#win-lose-text').textContent = 'You win!';
+        } else {
+        document.querySelector('#win-lose-text').textContent = 'You lose!';
+        }
+        document.querySelector('#result').classList.remove('hidden');
     }
+
 // add event listeners to the buttons
