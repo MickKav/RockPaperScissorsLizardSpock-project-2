1c7bc33c7143120715c61b250fd6b8caddc3f799
MickKav <>
2023-05-08 19:26:39 +0000

    Add event listeners for rock scissor and paper buttons

--- assets/script.js
+++ assets/script.js
@@ -62,4 +62,71 @@ function endGame() {
         document.querySelector('#result').classList.remove('hidden');
     }
 
-// add event listeners to the buttons
+    // add event listeners to the buttons
+let rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', () => {
+    if (lives > 0) {
+    let result = playRound('rock', computerPlay());
+    document.querySelector('#result-notice-text').textContent = result;
+    if (result === 'You win!') {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/thumbsup.png">';
+    } else if (result === 'You lose!') {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/thumbsdown.png">';
+    } else {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/scissors.png">';
+    }
+    updateLives();
+    if (lives === 0) {
+    endGame();
+    }
+    } else {
+    resetGame();
+    }
+});
+
+let paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', () => {
+    if (lives > 0) {
+    let result = playRound('paper', computerPlay());
+    document.querySelector('#result-notice-text').textContent = result;
+    if (result === 'You win!') {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/thumbsup.png">';
+    } else if (result === 'You lose!') {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/thumbsdown.png">';
+    } else {
+    document.querySelector('#result-image').innerHTML = '<img src="assets/rock.png">';
+    }
+    updateLives();
+    if (lives === 0) {
+    endGame();
+    }
+    } else {
+    resetGame();
+    }
+});
+
+
+let scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', () => {
+    if (lives > 0) {
+    let result = playRound('scissors', computerPlay());
+    document.querySelector('#result-notice-text').textContent = result;
+    if (result === 'You win!') {
+    document.querySelector('#result-image').innerHTML = '<img src="/assets/thumbsup.png">';
+    } else if (result === 'You lose!') {
+    document.querySelector('#result-image').innerHTML = '<img src="/assets/thumbsdown.png">';
+    } else {
+    document.querySelector('#result-image').innerHTML = '<img src="/assets/paper.png">';
+    }
+    updateLives();
+    if (lives === 0) {
+    endGame();
+    }
+    } else {
+    resetGame();
+    }
+});
+
+// add event listener to the reset button
+let resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', resetGame);
